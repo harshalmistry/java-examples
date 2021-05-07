cf7a344bd3d7a6a4152c7b4d9fcd7b7866b2507c
Harshal Mistry <er.harshalmistry@gmail.com>
2021-05-07 19:57:50 +0530

    hello world example

---
+++ src/main/java/basic/HelloWorld.java
@@ -0,0 +1,14 @@
+package basic;
+
+/**
+ * This is java hello world example.
+ * It simply prints String message to console.
+ *
+ * @author niharsh
+ * @created 07/05/2021 - 19:51
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java!");
+    }
+}
